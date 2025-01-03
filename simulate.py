498c7ad32780110193e42097fd22f7729c9bc122
Rachael Chertok <rachaelchertok@gmail.com>
2025-01-03 17:34:48 -0500

    Added sleep to see more of PyBullet sim

--- simulate.py
+++ simulate.py
@@ -1,5 +1,10 @@
 import pybullet as p
+import time
+
+t = 1/60
 physicsClient = p.connect(p.GUI)
 for i in range(0,1000):
+    print("Iteration: ", i)
+    time.sleep(t)
     p.stepSimulation()
 p.disconnect()
