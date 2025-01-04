b92f9fe468103bd61c7449be3685417a9dd8e0fd
Rachael Chertok <rachaelchertok@gmail.com>
2025-01-03 21:04:57 -0500

    Set up pyrosim in generate.py

---
+++ generate.py
@@ -0,0 +1,8 @@
+import pyrosim.pyrosim as pyrosim
+pyrosim.Start_SDF("box.sdf")
+
+#Stores a box with initial position x=0, y=0, z=0.5
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+
+#tells pyrosim to close the sdf file
+pyrosim.End() 
