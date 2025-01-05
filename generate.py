3e8cb48601556ebf129f392f13c02ec7a60425a7
Rachael Chertok <rachaelchertok@gmail.com>
2025-01-04 21:38:00 -0500

    Created a tower, where all blocks are the same size, directly on top of each other.

--- generate.py
+++ generate.py
@@ -6,17 +6,27 @@ length = 1
 width = 1
 height = 1
 
+x = 0
+y = 0
+z = 0.5
+
+'''
 x_box1 = 0
 y_box1 = 0
 z_box1 = 0.5
 
 x_box2 = 0
-y_box2 = 1
+y_box2 = 0
 z_box2 = 1.5
-#Stores a box with initial position x=0, y=0, z=0
-pyrosim.Send_Cube(name="Box", pos=[x_box1,y_box1,z_box1], size=[length,width,height])
+'''
 
-pyrosim.Send_Cube(name="Box2", pos=[x_box2,y_box2, z_box2], size=[length,width,height])
+for i in range(0,10):
+    #Stores a box with initial position x=0, y=0, z=0
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
+    z+=1
+    
+
+#pyrosim.Send_Cube(name="Box2", pos=[x_box2,y_box2, z_box2], size=[length,width,height])
 
 #tells pyrosim to close the sdf file
 pyrosim.End() 
