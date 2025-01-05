d71d52448a1e019c96c8457b91a77cd7739a43a6
Rachael Chertok <rachaelchertok@gmail.com>
2025-01-04 20:35:27 -0500

    Made it so that 1x2x3 block lays flat on floor.

--- generate.py
+++ generate.py
@@ -1,8 +1,16 @@
+#This file alters what is in the world
 import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("box.sdf")
 
-#Stores a box with initial position x=0, y=0, z=0.5
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+length = 1
+width = 2
+height = 3
+
+x = 0
+y = 0
+z = 1.5
+#Stores a box with initial position x=0, y=0, z=0
+pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
 
 #tells pyrosim to close the sdf file
 pyrosim.End() 
--- simulate.py
+++ simulate.py
@@ -1,8 +1,18 @@
+#This file alters how the world is simulated
 import pybullet as p
+import pybullet_data
 import time
 
 t = 1/60
 physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+#Setting up gravity in the world
+p.setGravity(0,0,-9.8)
+
+#Adding a floor
+planeId = p.loadURDF("plane.urdf")
 
 #Tells pybullet to read in the world described in box.sdf (which was created in generate.py)
 p.loadSDF("box.sdf")
