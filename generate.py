241a7a8c99cde7bcab8a4bf8d8a3a8e78dc4e363
Rachael Chertok <rachaelchertok@gmail.com>
2025-01-06 13:42:25 -0500

    Created Create_World() function and moved other lines into it

--- generate.py
+++ generate.py
@@ -1,20 +1,32 @@
 #This file alters what is in the world
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("world.sdf")
 
-length = 0.9
-width = 0.9
-height = 0.9
-
-x = 0
-y = 0
-z = 0.5
-
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
 
     
+
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+
+    length = 0.9
+    width = 0.9
+    height = 0.9
+
+    x = 0
+    y = 0
+    z = 0.5
+
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
+
+    #tells pyrosim to close the sdf file
+    pyrosim.End() 
+
+def main():
+    Create_World()
+    
+if __name__ == "__main__":
+    main()
     
 
-#tells pyrosim to close the sdf file
-pyrosim.End() 
+    
 
